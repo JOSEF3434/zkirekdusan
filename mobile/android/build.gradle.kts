f976a854e09b1d5062f22aee4e284ce48a89bc13
JOSEF3434 <jocieman@gmail.com>
2026-09-02 17:22:10 +0300

    chore: initialize Android project structure and project dependencies

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -20,11 +20,9 @@ subprojects {
 }
 
 subprojects {
-    // Apply JVM 17 target to all Kotlin compilation tasks across all Flutter plugins
-    plugins.withId("org.jetbrains.kotlin.android") {
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
+    val android = project.extensions.findByName("android")
+    if (android is com.android.build.gradle.BaseExtension) {
+        android.compileSdkVersion(36)
     }
 }
 
