038a823f4bab964fa2105421ecc640d1ef272559
JOSEF3434 <jocieman@gmail.com>
2026-09-18 11:33:28 +0300

    fix: improve Flutter installation check in build script

--- mobile/build_web.sh
+++ mobile/build_web.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+FLUTTER_DIR="$HOME/flutter"
 
-export PATH="$HOME/flutter/bin:$PATH"
+if [ ! -x "$FLUTTER_DIR/bin/flutter" ]; then
+  echo "Flutter not found. Installing Flutter..."
+  git clone https://github.com/flutter/flutter.git \
+    --depth 1 \
+    -b stable \
+    "$FLUTTER_DIR"
+else
+  echo "Flutter already exists. Reusing cached Flutter SDK."
+fi
 
-flutter doctor
+export PATH="$FLUTTER_DIR/bin:$PATH"
+
+flutter --version
+
+flutter config --enable-web
+
 flutter pub get
 
 flutter build web \
